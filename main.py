a42c857042d0813522d083974438aa3b2a0dced1
rushic24 <rushic24@gmail.com>
2022-05-17 01:20:39 -0400

    fc

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QMenu
+from time import sleep
 
 root = os.path.dirname(sys.argv[0])
 PDFJS = f"file://{os.path.abspath('./web/viewer.html')}"
@@ -17,8 +19,17 @@ class Window(QWebEngineView):
         super(Window, self).__init__()
         if len(sys.argv) > 1:
             self.load(QUrl.fromUserInput(f'{PDFJS}?file={PDF}'))
+            self.selectionChanged.connect(self.selection_changed)
         else:
             self.load(QUrl.fromUserInput(f"{PDFJS}?file=file://{root}/test.pdf"))
+    def selection_changed(self):
+        print("Selection changed:", self.selectedText())
+
+    # https://forum.qt.io/topic/128824/custom-context-menu-in-qtwebengineview-in-python
+    def contextMenuEvent(self, event):
+        self.menus = QMenu()
+        self.menus.addAction('Speak')
+        self.menus.popup(event.globalPos())
 
 if __name__ == '__main__':
 
