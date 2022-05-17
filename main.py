d277218c12d28dcd94e1d12ec2d1d0abb62d156e
rushic24 <rushic24@gmail.com>
2022-05-17 16:07:38 -0400

    added inference

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 from PyQt5.QtWidgets import QMenu
 from time import sleep
+import requests
 
 root = os.path.dirname(sys.argv[0])
 PDFJS = f"file://{os.path.abspath('./web/viewer.html')}"
@@ -23,13 +24,22 @@ class Window(QWebEngineView):
         else:
             self.load(QUrl.fromUserInput(f"{PDFJS}?file=file://{root}/test.pdf"))
     def selection_changed(self):
-        print("Selection changed:", self.selectedText())
+        # print("Selection changed:", self.selectedText())
+        pass
 
     # https://forum.qt.io/topic/128824/custom-context-menu-in-qtwebengineview-in-python
     def contextMenuEvent(self, event):
         self.menus = QMenu()
-        self.menus.addAction('Speak')
+        
+        speak_action = self.menus.addAction('Speak')
+        speak_action.triggered.connect(self.inference)
+        
         self.menus.popup(event.globalPos())
+
+    def inference(self):
+        text = self.self.selectedText()
+        r = requests.post('http://127.0.0.1:5000', data={'text': text})
+        print(r.json())
 
 if __name__ == '__main__':
 
